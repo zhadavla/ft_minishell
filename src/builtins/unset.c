590e51ea033faeb4ffbd2b6ed14f50e592480d3b
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-09-22 22:15:59 +0200

    unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -2,24 +2,26 @@
 
 void remove_env_node(t_env *env_head, t_env *to_remove)
 {
-	t_env *tmp;
-	t_env *prev;
+    t_env *tmp;
+    t_env *prev;
 
-	tmp = env_head;
-	prev = NULL;
-	while (tmp)
-	{
-		if (tmp == to_remove)
-		{
-			if (prev)
-				prev->next = tmp->next;
-			else
-				env_head = tmp->next;
+    tmp = env_head;
+    prev = NULL;
+    while (tmp)
+    {
+        if (tmp == to_remove)
+        {
+            if (prev)
+                prev->next = tmp->next;
+            else
+                env_head = tmp->next;
+			free(tmp->full_env);
+            free(tmp);
 			break ;
-		}
-		prev = tmp;
-		tmp = tmp->next;
-	}
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
 }
 
 int ft_unset(t_minishell *minishell)
@@ -40,13 +42,13 @@ int ft_unset(t_minishell *minishell)
 	key = cmd_node->cmd_full[1];
 	while (tmp)
 	{
+		prev = tmp;
+		fprintf(stderr, C_YELLOW "prev {%s}\n" C_RESET, tmp->full_env);
 		if (!ft_strncmp(key, tmp->full_env, ft_strlen(key)))
 		{
 			remove_env_node(minishell->env_list, tmp);
 			break ;
 		}
-		prev = tmp;
-		fprintf(stderr, C_YELLOW "%s\n" C_RESET, tmp->full_env);
 		tmp = tmp->next;
 	}
 	minishell->env = t_env_to_array(minishell->env_list);
